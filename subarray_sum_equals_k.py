baea97d760db155fd1b437b76398559c74d5046b
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-12-02 11:41:16 -0800

    Completed Hashing 2

---
+++ subarray_sum_equals_k.py
@@ -0,0 +1,19 @@
+# Leetcode on leet code:  Yes
+# Runtime complexity:     O(n)
+# Space complexity:       O(n)
+
+
+class Solution:
+    def subarraySum(self, nums, k):
+        d = {0:1}
+        Sum = 0
+        count = 0
+        for n in nums:
+            Sum += n
+            if Sum-k in d:
+                count += d[Sum-k]
+            if Sum in d:
+                d[Sum] += 1
+            else:
+                d[Sum] = 1      
+        return count
